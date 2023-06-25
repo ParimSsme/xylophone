f9d57f7d5ed91e9fa0ff4a6a7d5af518862f1631
ParimSsme <parima.ssme@gmail.com>
2023-06-25 11:26:29 +0430

    play all sounds on button click

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,48 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const XylophoneApp());
+  runApp(XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({super.key});
+
+  XylophoneApp({super.key});
+
+  final List<Color> colors = [
+    Colors.red,
+    Colors.orange,
+    Colors.yellow,
+    Colors.green,
+    Colors.teal,
+    Colors.blue,
+    Colors.purple,
+  ];
+
+  void playSound(int soundNumber){
+    final player = AudioCache();
+    player.play('note${soundNumber + 1}.wav');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              child: const Text('Click Me'),
-              onPressed: (){
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              for (var i = 0; i < colors.length; i++)
+                Expanded(
+                  child: TextButton(
+                    style: TextButton.styleFrom(backgroundColor: colors[i]),
+                    child: const Text(''),
+                    onPressed: (){
+                      playSound(i);
+                    },
+                  ),
+                ),
+            ],
           ),
         ),
       ),
